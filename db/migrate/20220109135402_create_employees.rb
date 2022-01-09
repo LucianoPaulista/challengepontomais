e5211ff97260567c1ea9b95c5649cc6b5d66a058
LucianoPaulista <lucianodiaspaulista@gmail.com>
2022-01-09 15:09:26 -0300

    fix: rename references

--- db/migrate/20220109135402_create_employees.rb
+++ db/migrate/20220109135402_create_employees.rb
@@ -4,8 +4,8 @@ class CreateEmployees < ActiveRecord::Migration[6.1]
       t.string :first_name
       t.string :last_name
       t.string :cpf
-      t.references :teams, null: false, foreign_key: true
-      t.references :job_titles, null: false, foreign_key: true
+      t.references :team, null: false, foreign_key: true
+      t.references :job_title, null: false, foreign_key: true
 
       t.timestamps
     end
