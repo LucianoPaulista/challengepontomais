ac47afc2f21295fbf6404172375f2a24785e1245
LucianoPaulista <lucianodiaspaulista@gmail.com>
2022-01-09 22:00:15 -0300

    test: added test for team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Create Tem' do
+    team = Team.create(name: "RH")
+    expect(team.name).to eq('RH')
+  end
 end
