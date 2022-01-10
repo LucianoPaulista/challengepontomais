8a72465ca54e656226fc87351b2e432e058ef578
LucianoPaulista <lucianodiaspaulista@gmail.com>
2022-01-09 22:00:24 -0300

    test: added test for job

--- spec/models/job_title_spec.rb
+++ spec/models/job_title_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe JobTitle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Create Tem' do
+    job_title = JobTitle.create(name: "Analista de Testes")
+    expect(job_title.name).to eq('Analista de Testes')
+  end
 end
