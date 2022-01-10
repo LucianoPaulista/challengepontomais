2917464d180a38b35561a55182a4d09e662f53cd
LucianoPaulista <lucianodiaspaulista@gmail.com>
2022-01-09 22:01:31 -0300

    build: added seed for Team, JobTitle and Employee

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Team.create([{name: "TI"}, {name: "TI2"}, {name: "TI3"} ])
+JobTitle.create([{ name: 'Desenvolvedor back-endimpr' }, { name: 'Desenvolvedor front-end' }, {name: 'Encantador de clientes' } ])
+Employee.create([{first_name: 'Pedro', last_name: 'Alves', cpf: '665.415.876-80', team_id: 2,  job_title_id: 3},
+                 {first_name: 'Tiago', last_name: 'Nogueira', cpf: '032.336.130-75', team_id: 1,  job_title_id: 1},
+                 {first_name: 'João', last_name: 'Neves', cpf: '461.636.517-23', team_id: 1,  job_title_id: 2 }])
